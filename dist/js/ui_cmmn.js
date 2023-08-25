cb0015f3f97fc2bd298fa44a5855037f5bdf74e0
limyoursun <limyoursun@gmail.com>
2023-08-25 14:07:00 +0900

    업무 파일 추가

--- dist/js/ui_cmmn.js
+++ dist/js/ui_cmmn.js
@@ -485,15 +485,6 @@ function setEditor() {
 }
 
 function setImageEditor() {
-  // Image editor
-  var imageEditor = new tui.ImageEditor('#image_editor', {
-    includeUI: {
-      //  theme: blackTheme, // or whiteTheme
-      initMenu: 'filter',
-      menuBarPosition: 'bottom',
-    },
-    usageStatistics: false,
-  })
 
   // 이미지 업로드 팝업 관련 요소 선택
   var imageEditor = null // Initialize Image Editor variable
@@ -511,10 +502,10 @@ function setImageEditor() {
       })
 
       // CKEditor의 내용에 Markdown 이미지 추가
-      const editorData = editor.getData().split('\n\n')
+      const editorData = editor.getData().split('\n')
       const row_num = editor.model.document.selection.getFirstPosition().path[0]
       editorData[row_num] = editorData[row_num] + markdownImage
-      editor.setData(editorData.join('\n\n'))
+      editor.setData(editorData.join('\n'))
       // 이미지 에디터 팝업 닫기
       imageEditorWrap.style.display = 'none'
     }
@@ -523,6 +514,83 @@ function setImageEditor() {
   cancelButton.addEventListener('click', function () {
     imageEditorWrap.style.display = 'none'
   })
+const locale_ru_RU = {
+  // override default English locale to your custom
+  Crop: 'Обзрезать',
+  'Delete-all': 'Удалить всё',
+  // etc...
+};
+
+const locale_zh = {
+  // override default English locale to your custom
+  Crop: "裁剪",
+  DeleteAll: "全部删除",
+  Delete: "删除",
+  Undo: "撤销",
+  Redo: "反撤销",
+  Reset: "重置",
+  Flip: "镜像",
+  Rotate: "旋转",
+  Draw: "画",
+  Shape: "形状标注",
+  Icon: "图标标注",
+  Text: "文字标注",
+  Mask: "遮罩",
+  Filter: "滤镜",
+  Bold: "加粗",
+  Italic: "斜体",
+  Underline: "下划线",
+  Left: "左对齐",
+  Center: "居中",
+  Right: "右对齐",
+  Color: "颜色",
+  "Text size": "字体大小",
+  Custom: "自定义",
+  Square: "正方形",
+  Apply: "应用",
+  Cancel: "取消",
+  "Flip X": "X 轴",
+  "Flip Y": "Y 轴",
+  Range: "区间",
+  Stroke: "描边",
+  Fill: "填充",
+  Circle: "圆",
+  Triangle: "三角",
+  Rectangle: "矩形",
+  Free: "曲线",
+  Straight: "直线",
+  Arrow: "箭头",
+  "Arrow-2": "箭头2",
+  "Arrow-3": "箭头3",
+  "Star-1": "星星1",
+  "Star-2": "星星2",
+  Polygon: "多边形",
+  Location: "定位",
+  Heart: "心形",
+  Bubble: "气泡",
+  "Custom icon": "自定义图标",
+  "Load Mask Image": "加载蒙层图片",
+  Grayscale: "灰度",
+  Blur: "模糊",
+  Sharpen: "锐化",
+  Emboss: "浮雕",
+  "Remove White": "除去白色",
+  Distance: "距离",
+  Brightness: "亮度",
+  Noise: "噪音",
+  "Color Filter": "彩色滤镜",
+  Sepia: "棕色",
+  Sepia2: "棕色2",
+  Invert: "负片",
+  Pixelate: "像素化",
+  Threshold: "阈值",
+  Tint: "色调",
+  Multiply: "正片叠底",
+  Blend: "混合色",
+  // etc...
+};
+
+
 
   var editableDiv = document.querySelector('.ck-editor__editable')
     editableDiv.addEventListener('paste', function (event) {
@@ -541,10 +609,11 @@ function setImageEditor() {
                     path: event.target.result,
                     name: 'PastedImage',
                   },
-                  initMenu: 'filter',
+                  initMenu: 'draw',
                   menuBarPosition: 'bottom',
                 },
                 usageStatistics: false,
+                locale: locale_zh,
               })
               window.onresize = function () {
                 imageEditor.ui.resizeEditor()
